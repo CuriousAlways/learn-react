81ee4fe14934e55f728ba7ea400055457159694f
curiousAlways <razakhanjmi@gmail.com>
2023-12-31 13:53:17 +0530

    states and event handlers

--- README.md
+++ README.md
@@ -27,4 +27,69 @@ const Hello = (props) => {
 
 exports default Hello;
 ```
+> `props` are used to pass data b/w components.
 
+> **component functions**  are functions defined inside a component. advantage of this component function is that we don't have to explicitly pass any attributes it would have access to props.
+
+```js
+// component function
+const Hello = (props) => {
+  // Inside this componet function we can access props directly
+  const bornYear = () => {    const yearNow = new Date().getFullYear()    return yearNow - props.age  }
+
+  return (
+    ...
+  )
+}
+```
+
+## React hooks
+>-  hooks may only be called from the inside of a function body that defines a React.
+>- hooks must not be called from inside of a loop, a conditional expression, or any place that is not a function defining a component
+
+
+- [`useState`](https://react.dev/learn/state-a-components-memory) <br>
+  - This hook is used to maintain state of a react component.
+  - When the value of state changes then components and its child is re-rendered
+  - A component can have any number of `useState` hook
+
+```js
+import {useState} from 'react';
+
+const Hello => () => {
+  const [counter, setCounter] = useState(0);
+
+  /****************************************************
+   * changes counter value by one after 1s of its
+   * execution which triggers re-render of this Hello
+   * component. After this setTimeOut is executed once
+   * again to update counter value after 1sec and this
+   * loop continues infinetly......
+   ***************************************************/
+  setTimeOut(()=> setCounter(counter + 1), 1000)
+
+  return (<div> { counter } </div>);
+}
+```
+
+  - `useState` returns array with 2 element, 1st is element with current value and 2nd element is function to update state. **NEVER UPDDATE DIRECTLY ALWAYS USE STATE UPDATE FUNCTION TO UPDATE STATE BY SETTING THE STATE TO NEW OBJECT**.
+
+```js
+import {useState} from 'react';
+//....
+  const [counter, setCounter] = useState({left: 0, right: 1});
+  //....
+
+  // INVALID way of updating state
+  // counter.left++;
+  // setCounter(counter)
+
+  // VALID
+  newCounter = { left: counter.left + 1, right: counter.right }
+  setCounter(newCounter);
+  // Alternative
+  setCounter({...counter, left: counter.left + 1 })
+
+
+//....
+```
--- introduction-to-react/src/App.jsx
+++ introduction-to-react/src/App.jsx
@@ -1,21 +1,82 @@
+import { useState } from "react";
+
 const App = () => {
+  const [leftCounter, setLeftCounter] = useState(0);
+  const [rightCounter, setRightCounter] = useState(0);
+  const [allClicks, setAllClicks] = useState([]);
+  const [totalClicks, setTotalClicks] = useState(0);
+
+  // setTimeout(()=> { setCounter(counter + 1) }, 1000);
+  // const handleClick = () => {
+  //   console.log('clicked...')
+  // }
+
+  // console.log('redering..', counter);
+
+  // const increaseCounter = () => setCounter(counter + 1);
+  // const decreaseCounter = () => setCounter(counter - 1);
+  // const zeroCounter     = () => setCounter(0);
+
+  const increaseLeftCounter = () => {
+    setLeftCounter(leftCounter + 1);
+    updateTotalClicks();
+    setAllClicks(allClicks.concat('L'));
+  }
+
+  const increaseRightCounter = () => {
+    setRightCounter(rightCounter + 1);
+    updateTotalClicks();
+    setAllClicks(allClicks.concat('R'));
+  }
+
+  const updateTotalClicks = () => {
+    setTotalClicks(totalClicks + 1)
+  }
+
+
   return (
     <div>
-      <h1>Greetings</h1>
-      <Hello name="raza"/>
-      <Hello name={"ejaz"} />
-      <Hello name={"farhan"} />
+      <Display counter={leftCounter} />
+      <Button text="plus left counter" onClick={increaseLeftCounter} />
+      <Display counter={rightCounter} />
+      <Button text="plus right counter" onClick={increaseRightCounter} />
+      {/* <p> all clicks = { allClicks.join(', ')}</p> */}
+      <History allClicks={allClicks} />
+      <p> total clicks = {totalClicks} </p>
+      {/* <Button text="zero" onClick={zeroCounter} /> */}
     </div>
   )
+};
+
+const Display = ({counter}) => {
+  return <div>{counter}</div>;
 }
 
-const Hello = (props) => {
-  console.log(props)
+const Button = ({text, onClick}) => {
+  return <button onClick={onClick}>{text}</button>;
+}
+
+const History = ({allClicks}) => {
+  if(allClicks.length === 0) {
+    return (<p>Application is used by pressing buttons</p>);
+  }
+
+  return (<p>Click History: {allClicks.join(', ')}</p>);
+
+}
+
+
+const Hello = ({ name, age }) => {
+  const bornYear = () => new Date().getFullYear() - age;
+
   return (
     <div>
-      <p> Hello {props.name}! </p>
+      <p>
+        Hello {name}, you are {age} years old
+      </p>
+      <p>So you were probably born in {bornYear()}</p>
     </div>
-  );
+  )
 }
 
 export default App
