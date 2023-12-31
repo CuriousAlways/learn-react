4f6eaf6214431d0c47eb4b24184d1db9dd6d75fd
curiousAlways <razakhanjmi@gmail.com>
2023-12-31 13:53:48 +0530

    1.6: unicafe step1

---
+++ part-1/unicafe/unicafe/src/App.jsx
@@ -0,0 +1,38 @@
+import { useState } from "react";
+
+const App = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const goodReviewHandler = () => setGood(good+1);
+  const neutralReviewHandler = () => setNeutral(neutral + 1);
+  const badReviewHandler = () => setBad(bad + 1);
+
+  return (
+  <div>
+    <h2>Give feedback</h2>
+    <div>
+      <Button text='good' clickHandler={goodReviewHandler} />
+      <Button text='neutral' clickHandler={neutralReviewHandler} />
+      <Button text='bad' clickHandler={badReviewHandler} />
+    </div>
+    <div>
+      <h3>Statistics</h3>
+      <p>
+        good: {good} <br/>
+        neutral: {neutral} <br/>
+        bad: {bad}
+      </p>
+    </div>
+  </div>
+  );
+}
+
+const Button = ({text, clickHandler}) => {
+  return (
+    <button onClick={clickHandler}>{text}</button>
+  )
+}
+
+export default App;
