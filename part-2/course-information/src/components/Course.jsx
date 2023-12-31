b7134ced1fc742f38298bd9dd388ff403bfbc80a
curiousAlways <razakhanjmi@gmail.com>
2023-12-31 18:54:48 +0530

    2.2: Course information step7

--- part-2/course-information/src/components/Course.jsx
+++ part-2/course-information/src/components/Course.jsx
@@ -1,12 +1,14 @@
 import Header from "./Header";
 import Content from "./Content";
+import ExcercisesCount from "./ExcercisesCount";
 
 const Course = ({course}) => {
-
+  const totalNoOfExcercise = course.parts.reduce((sum, part) => sum + part.exercises, 0)
   return (
   <div>
     <Header name={course.name} />
     <Content parts={course.parts} />
+    <ExcercisesCount excerciseCount={totalNoOfExcercise} />
   </div>
   );
 }
---
+++ part-2/course-information/src/components/ExcercisesCount.jsx
@@ -0,0 +1,7 @@
+const ExcercisesCount = ({ excerciseCount }) => {
+  return (
+    <h4>total {excerciseCount} of excercises.</h4>
+  );
+}
+
+export default ExcercisesCount;
